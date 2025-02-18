d5f2c1a1bcf479a8a521dcb5dd04717061c967a1
thirumurugan7 <thiru82003@gmail.com>
2025-02-19 03:35:39 +0530

    added onlyowner

--- contracts/mocks/MockERC20.sol
+++ contracts/mocks/MockERC20.sol
@@ -2,11 +2,11 @@
 pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MockERC20 is ERC20 {
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract MockERC20 is ERC20, Ownable {
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
 
-    function mint(address to, uint256 amount) external {
+    function mint(address to, uint256 amount) onlyOwner external {
         _mint(to, amount);
     }
 }
